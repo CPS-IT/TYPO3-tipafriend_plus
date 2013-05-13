63155adf3508202b3231a18655dc95dc926e97ea
Nicole Cordes <cordes@cps-it.de>
2013-05-13 20:19:08 +0200

    Release of tipafriend_plus 0.1.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tipafriend_plus".
  *
- * Auto generated 13-05-2013 19:57
+ * Auto generated 13-05-2013 20:11
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -12,10 +12,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Tip-A-Friend Plus',
-	'description' => 'Send a tip (link to typo3-page) to somebody',
+	'description' => 'Send a tip (link to typo3-page) to somebody. Based on Tip-A-Friend.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.1.1',
+	'version' => '0.1.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"7f67";s:12:"ext_icon.gif";s:4:"3b63";s:17:"ext_localconf.php";s:4:"92c0";s:14:"ext_tables.php";s:4:"7d2b";s:28:"ext_typoscript_constants.txt";s:4:"6d7b";s:24:"ext_typoscript_setup.txt";s:4:"3fd8";s:15:"flexform_ds.xml";s:4:"38e5";s:13:"locallang.xml";s:4:"6037";s:16:"locallang_db.xml";s:4:"67b5";s:17:"locallang_tca.xml";s:4:"0f54";s:10:"README.txt";s:4:"9fa9";s:14:"doc/manual.sxw";s:4:"eb20";s:19:"doc/wizard_form.dat";s:4:"9960";s:20:"doc/wizard_form.html";s:4:"b82d";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:35:"pi1/class.tx_tipafriendplus_pi1.php";s:4:"cb09";s:43:"pi1/class.tx_tipafriendplus_pi1_wizicon.php";s:4:"c8fe";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"901d";s:28:"pi1/tipafriend_plus_css.tmpl";s:4:"9aa6";s:23:"res/tipafriend_plus.css";s:4:"9608";}',
+	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"7f67";s:12:"ext_icon.gif";s:4:"3b63";s:17:"ext_localconf.php";s:4:"92c0";s:14:"ext_tables.php";s:4:"7d2b";s:28:"ext_typoscript_constants.txt";s:4:"6d7b";s:24:"ext_typoscript_setup.txt";s:4:"3fd8";s:15:"flexform_ds.xml";s:4:"38e5";s:13:"locallang.xml";s:4:"6037";s:16:"locallang_db.xml";s:4:"67b5";s:17:"locallang_tca.xml";s:4:"0f54";s:10:"README.txt";s:4:"9fa9";s:14:"doc/manual.sxw";s:4:"eb20";s:19:"doc/wizard_form.dat";s:4:"9960";s:20:"doc/wizard_form.html";s:4:"b82d";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:35:"pi1/class.tx_tipafriendplus_pi1.php";s:4:"cb09";s:43:"pi1/class.tx_tipafriendplus_pi1_wizicon.php";s:4:"c8fe";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"faf1";s:28:"pi1/tipafriend_plus_css.tmpl";s:4:"9aa6";s:23:"res/tipafriend_plus.css";s:4:"9608";}',
 );
 
 ?>
