dc9b3a1e8e9b01a7b9df6b6e0db06fa6ef30e68c
Nicole Cordes <cordes@cps-it.de>
2017-07-24 16:28:37 +0200

    [RELEASE] Release of tipafriend_plus 0.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "tipafriend_plus".
  *
- * Auto generated 26-02-2014 23:19
+ * Auto generated 24-07-2017 16:27
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Obsolete - please do not use this extension any longer! Send a tip (link to typo3-page) to somebody. Based on Tip-A-Friend.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"0751";s:12:"ext_icon.gif";s:4:"3b63";s:17:"ext_localconf.php";s:4:"9c3a";s:14:"ext_tables.php";s:4:"593b";s:28:"ext_typoscript_constants.txt";s:4:"6d7b";s:24:"ext_typoscript_setup.txt";s:4:"3fd8";s:15:"flexform_ds.xml";s:4:"85c6";s:13:"locallang.xml";s:4:"6037";s:16:"locallang_db.xml";s:4:"67b5";s:17:"locallang_tca.xml";s:4:"41a2";s:14:"doc/manual.sxw";s:4:"63ac";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:35:"pi1/class.tx_tipafriendplus_pi1.php";s:4:"49a6";s:43:"pi1/class.tx_tipafriendplus_pi1_wizicon.php";s:4:"7529";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"23d6";s:28:"pi1/tipafriend_plus_css.tmpl";s:4:"be94";s:23:"res/tipafriend_plus.css";s:4:"f41c";}',
+	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"60f8";s:12:"ext_icon.gif";s:4:"3b63";s:17:"ext_localconf.php";s:4:"df8c";s:14:"ext_tables.php";s:4:"593b";s:28:"ext_typoscript_constants.txt";s:4:"312f";s:24:"ext_typoscript_setup.txt";s:4:"f97e";s:15:"flexform_ds.xml";s:4:"85c6";s:13:"locallang.xml";s:4:"6037";s:16:"locallang_db.xml";s:4:"67b5";s:17:"locallang_tca.xml";s:4:"9bf1";s:14:"doc/manual.sxw";s:4:"63ac";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:35:"pi1/class.tx_tipafriendplus_pi1.php";s:4:"49a6";s:43:"pi1/class.tx_tipafriendplus_pi1_wizicon.php";s:4:"7529";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"23d6";s:28:"pi1/tipafriend_plus_css.tmpl";s:4:"dbc6";s:23:"res/tipafriend_plus.css";s:4:"1268";}',
 	'suggests' => array(
 	),
 );
